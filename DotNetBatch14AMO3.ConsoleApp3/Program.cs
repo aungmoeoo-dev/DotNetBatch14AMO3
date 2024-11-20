6e67508bdd1ff07c51475cbdfa26e56f2609351c
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-11-20 23:37:35 +0630

    testing commit

--- DotNetBatch14AMO3.ConsoleApp3/Program.cs
+++ DotNetBatch14AMO3.ConsoleApp3/Program.cs
@@ -5,21 +5,21 @@ using DotNetBatch14AMO3.ConsoleApp3.AdoDotNetExamples;
 using DotNetBatch14AMO3.ConsoleApp3.DapperExamples;
 using DotNetBatch14AMO3.ConsoleApp3.EFCoreExamples;
 
-//AdoDotNetExample adoDotNetExample = new();
+AdoDotNetExample adoDotNetExample = new();
 //adoDotNetExample.Read();
 //adoDotNetExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C9");
 //adoDotNetExample.Create("AdoDotNet Create Test title", "AdoDotNet Create Test Author", "AdoDotNet Create Test Content");
 //adoDotNetExample.Update("FFFE1C18-22F0-4804-999F-A27377863DCD", "Ado updated title", "Ado updated Author", "Ado updated content");
 //adoDotNetExample.Delete("FFFE1C18-22F0-4804-999F-A27377863DCD");
 
-//DapperExample dapperExample = new();
+DapperExample dapperExample = new();
 //dapperExample.Read();
 //dapperExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C8");
 //dapperExample.Create("Dapper Create Test title", "Dapper Create Test Author", "Dapper Create Test Content");
 //dapperExample.Update("D384129B-6B07-4E59-9B19-1E06EA418A72", "Dapper updated title", "Dapper updated Author", "Dapper updated content");
 //dapperExample.Delete("D384129B-6B07-4E59-9B19-1E06EA418A72");
 
-//EFCoreExample eFCoreExample = new();
+EFCoreExample eFCoreExample = new();
 //eFCoreExample.Read();
 //eFCoreExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C9");
 //eFCoreExample.Create("EFCore create test 1", "EFCore test author", "EFCore test content");
